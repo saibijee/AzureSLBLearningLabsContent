83e5544de27c4e417a2aad5c3d5e4277abfc6c6d
saibijee <45789154+saibijee@users.noreply.github.com>
2020-01-22 12:01:59 +0000

    Update SLBProbeDownScenario001.ps1

--- SLBProbeDownScenario001.ps1
+++ SLBProbeDownScenario001.ps1
@@ -1,7 +1,8 @@
 Add-WindowsFeature Web-Server;
 Add-Content -Path "C:\inetpub\wwwroot\Default.htm" -Value $($env:computername)
 $action = New-ScheduledTaskAction -Execute 'Powershell.exe' -Argument '-command " $rule = Get-NetFirewallRule -Direction Inbound -DisplayGroup \"world*\";if(($rule).action -eq \"Allow\"){Set-NetFirewallRule -Direction Inbound -Action Block -DisplayGroup \"world*\"} else {Set-NetFirewallRule -Direction Inbound -DisplayGroup \"World*\" -Action Allow}"'
-$trigger =  New-ScheduledTaskTrigger -Daily -At ((get-date).AddMinutes(10))
+$mins = 20,30,40,50,60 | Get-Random
+$trigger =  New-ScheduledTaskTrigger -Daily -At ((get-date).AddMinutes($mins))
 $task = Register-ScheduledTask -Action $action -Trigger $trigger -TaskName "Port80Toggler" -Description "Port80Toggler" -User "NT Authority\System"
 $task.Triggers.Repetition.Duration = "P5D"
 $task.Triggers.Repetition.Interval = "PT06M"
