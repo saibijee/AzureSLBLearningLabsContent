4cc01cafdc9c47462ee64c7b1019012e49ce2def
carlost-MSFT <carlost@microsoft.com>
2023-02-23 18:26:23 -0800

    Update SLBProbeDownScenario001.ps1

--- SLBProbeDownScenario001.ps1
+++ SLBProbeDownScenario001.ps1
@@ -1,5 +1,7 @@
 Add-WindowsFeature Web-Server;
 Add-Content -Path "C:\inetpub\wwwroot\Default.htm" -Value $($env:computername)
+New-Item -ItemType Directory c:\inetpub\wwwroot\probe
+Add-Content -Path "C:\inetpub\wwwroot\probe\Default.htm" -Value $($env:computername),PROBE
 $action = New-ScheduledTaskAction -Execute 'Powershell.exe' -Argument '-command " $rule = Get-NetFirewallRule -Direction Inbound -DisplayGroup \"world*\";if(($rule).action -eq \"Allow\"){Set-NetFirewallRule -Direction Inbound -Action Block -DisplayGroup \"world*\"} else {Set-NetFirewallRule -Direction Inbound -DisplayGroup \"World*\" -Action Allow}"'
 $mins = 20,30,40,50,60 | Get-Random
 $trigger =  New-ScheduledTaskTrigger -Daily -At ((get-date).AddMinutes($mins))
